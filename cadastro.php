306044dc0bac22eee8c97eb844e6cd773f425997
MeninoFranca <baueremil9@gmail.com>
2024-05-31 23:28:17 -0300

    conexao com o banco

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,12 @@
 <?php
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-    }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servername = "127.0.0.1:3306";
+    $username = "rooot";
+    $password = "kSKZLaB2>";
+    $dbname = "u721539099_Gestaohora";
+
+    
+}
 ?>
