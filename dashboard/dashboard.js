91c6963c6a4b793ad4fe02cb282654ea30e113aa
MeninoFranca <baueremil9@gmail.com>
2024-06-01 13:43:00 -0300

    função para pegar hora e data atuais em js + modelagem painel inserção

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -7,3 +7,15 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+function updateDateTime() {
+    const now = new Date();
+    const options = {
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
+        hour: '2-digit', minute: '2-digit', second: '2-digit'
+    };
+    const formattedDate = now.toLocaleDateString('pt-BR', options);
+    document.getElementById('datetime').innerText = formattedDate;
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
