af4706f11f8fb10a95b7e1b8fc46956689beaf6b
MeninoFranca <baueremil9@gmail.com>
2024-05-31 23:38:21 -0300

    teste de cadastro

--- cadastro.php
+++ cadastro.php
@@ -7,6 +7,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = "kSKZLaB2>";
     $dbname = "u721539099_Gestaohora";
 
-    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+     
+     if ($conn->connect_error) {
+        die("Erro de conexão: " . $conn->connect_error);
+    }
+
+    $username = $_POST['Username'];
+    $password = $_POST['Password'];
+
+    $sql = "INSERT INTO Usuarios (username, senha) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        $response = array('Cadastrado com sucesso!');
+        echo json_encode($response);
+    } else {
+        $response = array('error' => 'Erro ao cadastrar: ' . $conn->error);
+        echo json_encode($response);
+    }
+
+    $conn->close();
+} else {
+    $response = array('error' => 'Método inválido');
+    echo json_encode($response);
 }
 ?>
