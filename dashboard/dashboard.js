6eac70980bda9c831dfc4d8e079b4b8a2a7282c0
MeninoFranca <baueremil9@gmail.com>
2024-06-01 14:00:38 -0300

    Implementando localStorage para deixar os dados pré definido para quando usuário sair e retornar ao site

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -8,27 +8,48 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Define a data atual ao carregar a página
     const dataInput = document.getElementById('data');
+    const entradaInput = document.getElementById('entrada');
+    const inicioAlmocoInput = document.getElementById('inicio_almoco');
+    const fimAlmocoInput = document.getElementById('fim_almoco');
+    const saidaInput = document.getElementById('saida');
+
     const today = new Date().toISOString().split('T')[0];
     dataInput.value = today;
+
+    
+    if (localStorage.getItem('entrada')) {
+        entradaInput.value = localStorage.getItem('entrada');
+    }
+    if (localStorage.getItem('inicio_almoco')) {
+        inicioAlmocoInput.value = localStorage.getItem('inicio_almoco');
+    }
+    if (localStorage.getItem('fim_almoco')) {
+        fimAlmocoInput.value = localStorage.getItem('fim_almoco');
+    }
+    if (localStorage.getItem('saida')) {
+        saidaInput.value = localStorage.getItem('saida');
+    }
 
     let clickCount = 0;
 
     document.getElementById('registrar-horario').addEventListener('click', () => {
         clickCount++;
         const now = new Date();
-        const timeString = now.toTimeString().split(' ')[0].substring(0, 5); // Formato HH:MM
+        const timeString = now.toTimeString().split(' ')[0].substring(0, 5); 
 
-        if (clickCount === 1) {
-            document.getElementById('entrada').value = timeString;
-        } else if (clickCount === 2) {
-            document.getElementById('inicio_almoco').value = timeString;
-        } else if (clickCount === 3) {
-            document.getElementById('fim_almoco').value = timeString;
-        } else if (clickCount === 4) {
-            document.getElementById('saida').value = timeString;
-            // Submeter o formulário após preencher o campo de saída
+        if (!entradaInput.value) {
+            entradaInput.value = timeString;
+            localStorage.setItem('entrada', timeString);
+        } else if (!inicioAlmocoInput.value) {
+            inicioAlmocoInput.value = timeString;
+            localStorage.setItem('inicio_almoco', timeString);
+        } else if (!fimAlmocoInput.value) {
+            fimAlmocoInput.value = timeString;
+            localStorage.setItem('fim_almoco', timeString);
+        } else if (!saidaInput.value) {
+            saidaInput.value = timeString;
+            localStorage.setItem('saida', timeString);
             document.querySelector('form').submit();
         }
     });
