22795f99815c75e2e4509d50b38836bc0fd758ee
MeninoFranca <baueremil9@gmail.com>
2024-06-01 13:57:26 -0300

    aplicando funcionalidade

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -7,15 +7,29 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Define a data atual ao carregar a página
+    const dataInput = document.getElementById('data');
+    const today = new Date().toISOString().split('T')[0];
+    dataInput.value = today;
 
-function updateDateTime() {
-    const now = new Date();
-    const options = {
-        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
-        hour: '2-digit', minute: '2-digit', second: '2-digit'
-    };
-    const formattedDate = now.toLocaleDateString('pt-BR', options);
-    document.getElementById('datetime').innerText = formattedDate;
-}
-setInterval(updateDateTime, 1000);
-updateDateTime();
+    let clickCount = 0;
+
+    document.getElementById('registrar-horario').addEventListener('click', () => {
+        clickCount++;
+        const now = new Date();
+        const timeString = now.toTimeString().split(' ')[0].substring(0, 5); // Formato HH:MM
+
+        if (clickCount === 1) {
+            document.getElementById('entrada').value = timeString;
+        } else if (clickCount === 2) {
+            document.getElementById('inicio_almoco').value = timeString;
+        } else if (clickCount === 3) {
+            document.getElementById('fim_almoco').value = timeString;
+        } else if (clickCount === 4) {
+            document.getElementById('saida').value = timeString;
+            // Submeter o formulário após preencher o campo de saída
+            document.querySelector('form').submit();
+        }
+    });
+});
