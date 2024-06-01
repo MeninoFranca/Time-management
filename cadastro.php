ab9732d31492765c2d01763b5a5d748fa69b35e3
MeninoFranca <baueremil9@gmail.com>
2024-05-31 23:43:33 -0300

    sucesso no cadastro + criaçao da autenticação

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "127.0.0.1:3306";
-    $username = "rooot";
+    $username = "u721539099_rooot";
     $password = "kSKZLaB2>";
     $dbname = "u721539099_Gestaohora";
 
