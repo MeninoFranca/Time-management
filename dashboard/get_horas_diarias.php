97e30513a3278cd4f63e7a0800cb26c8612a30a9
MeninoFranca <baueremil9@gmail.com>
2024-06-02 16:39:09 -0300

    consulta sql diaria aprimorada, mais precisa!

--- dashboard/get_horas_diarias.php
+++ dashboard/get_horas_diarias.php
@@ -16,24 +16,35 @@ if ($conn->connect_error) {
 
 $usuario_id = $_SESSION['usuario_id'];
 
-$sql = "SELECT data, entrada, inicio_almoco, fim_almoco, saida 
-        FROM horarios 
+
+$sql = "SELECT 
+            DATE_FORMAT(data, '%d/%m/%Y') AS dia,
+            SEC_TO_TIME(SUM(
+                TIME_TO_SEC(TIMEDIFF(IFNULL(saida, CURRENT_TIME), entrada)) - 
+                IFNULL(TIME_TO_SEC(TIMEDIFF(IFNULL(fim_almoco, CURRENT_TIME), inicio_almoco)), 0)
+            )) AS horas_trabalhadas
+        FROM RegistroHorario
         WHERE MONTH(data) = MONTH(CURRENT_DATE()) 
         AND YEAR(data) = YEAR(CURRENT_DATE()) 
-        AND usuario_id = ?";
-
+        AND DAYOFWEEK(data) != 1
+        AND usuario_id = ?
+        GROUP BY dia
+        ORDER BY data";
+        
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $usuario_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$horarios = [];
+$data = [];
+
 while ($row = $result->fetch_assoc()) {
-    $horarios[] = $row;
+    $data[$row['dia']] = $row['horas_trabalhadas'];
 }
+
+echo json_encode($data);
 
 $stmt->close();
 $conn->close();
 
-echo json_encode($horarios);
 ?>
