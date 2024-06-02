ae69a1ccd227867b158133ec0c95991aa8266d43
MeninoFranca <baueremil9@gmail.com>
2024-06-02 12:41:33 -0300

    insercao de hora no banco, fazendo a conexão com banco e checando se os dados foram recebidos

--- dashboard/insercao_horario.php
+++ dashboard/insercao_horario.php
@@ -1,35 +1,20 @@
 <?php
 include 'autentica.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername = "127.0.0.1:3306";
-    $username = "u721539099_rooot";
-    $password = "kSKZLaB2>";
-    $dbname = "u721539099_Gestaohora";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['data'], $_POST['entrada'], $_POST['inicio_almoco'], $_POST['fim_almoco'], $_POST['saida'])) {
+    $servername = "127.0.0.1";
+    $username = "u721539099_rooot"; 
+    $password = "kSKZLaB2>"; 
+    $dbname = "u721539099_Gestaohora"; 
+
+  
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Erro de conexão: " . $conn->connect_error);
     }
 
-    $data = $_POST['data'];
-    $entrada = $_POST['entrada'];
-    $inicio_almoco = $_POST['inicio_almoco'];
-    $fim_almoco = $_POST['fim_almoco'];
-    $saida = $_POST['saida'];
 
-    $sql = "INSERT INTO horarios (data, entrada, inicio_almoco, fim_almoco, saida) VALUES (?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssss", $data, $entrada, $inicio_almoco, $fim_almoco, $saida);
-
-    if ($stmt->execute()) {
-        echo "Horários registrados com sucesso!";
-        echo "<script>localStorage.clear();</script>";
-    } else {
-        echo "Erro ao registrar horários: " . $stmt->error;
-    }
-    
-    
-    $stmt->close();
-    $conn->close();
 }
 ?>
